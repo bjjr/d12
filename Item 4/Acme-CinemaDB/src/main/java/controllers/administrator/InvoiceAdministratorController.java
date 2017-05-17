ac27ffd9a258b16a22a31f73c56d8bf44403e202
bernar95 <berny.sevillista@gmail.com>
2017-05-17 20:25:53 +0200

    Some changes added to invoice controller of administrator

--- Item 4/Acme-CinemaDB/src/main/java/controllers/administrator/InvoiceAdministratorController.java
+++ Item 4/Acme-CinemaDB/src/main/java/controllers/administrator/InvoiceAdministratorController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.CampaignService;
@@ -74,41 +73,28 @@ public class InvoiceAdministratorController extends AbstractController {
 	@RequestMapping(value = "/create", method = RequestMethod.POST, params = "save")
 	public ModelAndView save(final Invoice invoice, final BindingResult binding) {
 		ModelAndView result;
+		Invoice reconstructed;
+		final Collection<Campaign> unpaidCampaigns;
+
+		reconstructed = this.invoiceService.reconstruct(invoice, binding);
+		unpaidCampaigns = this.campaignService.findUnpaidCampaigns();
 
 		if (binding.hasErrors()) {
 			result = new ModelAndView("invoice/create");
-			result.addObject("invoice", invoice);
+			result.addObject("invoice", reconstructed);
+			result.addObject("campaigns", unpaidCampaigns);
 		} else
 			try {
-				this.invoiceService.save(invoice);
+				this.invoiceService.save(reconstructed);
 				result = new ModelAndView("redirect:list.do");
 			} catch (final Throwable th) {
 				result = new ModelAndView("invoice/create");
-				result.addObject("invoice", invoice);
+				result.addObject("invoice", reconstructed);
 				result.addObject("message", "misc.commit.error");
+				result.addObject("campaigns", unpaidCampaigns);
 			}
 
 		return result;
 	}
 
-	//Set paid -----------------------------------------
-
-	@RequestMapping(value = "/setPaid", method = RequestMethod.GET)
-	public ModelAndView cancel(@RequestParam final int invoiceId) {
-		ModelAndView result;
-		Invoice invoice;
-
-		invoice = this.invoiceService.findOne(invoiceId);
-
-		try {
-			this.invoiceService.setPaid(invoice);
-			result = new ModelAndView("redirect:list.do");
-			result.addObject("messageStatus", "misc.commit.ok");
-		} catch (final Throwable oops) {
-			result = new ModelAndView("redirect:list.do");
-			result.addObject("messageStatus", "misc.commit.error");
-		}
-
-		return result;
-	}
 }
