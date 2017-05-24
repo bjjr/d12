7a66405c15210675bf0d6546c70c15ecdbbb4e09
bernar95 <berny.sevillista@gmail.com>
2017-05-24 12:19:59 +0200

    Banner funcionality added to welcome

--- Item 4/Acme-CinemaDB/src/main/java/controllers/WelcomeController.java
+++ Item 4/Acme-CinemaDB/src/main/java/controllers/WelcomeController.java
@@ -13,14 +13,26 @@ package controllers;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.ActorService;
+import services.CampaignService;
+
 @Controller
 @RequestMapping("/welcome")
 public class WelcomeController extends AbstractController {
+
+	//Services --------------------------------------------------------------
+
+	@Autowired
+	private CampaignService	campaignService;
+	@Autowired
+	private ActorService	actorService;
+
 
 	// Constructors -----------------------------------------------------------
 
@@ -35,13 +47,21 @@ public class WelcomeController extends AbstractController {
 		ModelAndView result;
 		SimpleDateFormat formatter;
 		String moment;
-
+		String banner;
+		String name;
+		try {
+			name = this.actorService.findByPrincipal().getName();
+		} catch (final Exception e) {
+			name = "";
+		}
 		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		moment = formatter.format(new Date());
+		banner = this.campaignService.displayBanner();
 
 		result = new ModelAndView("welcome/index");
+		result.addObject("name", name);
 		result.addObject("moment", moment);
-		result.addObject("conf", conf);
+		result.addObject("banner", banner);
 
 		return result;
 	}
