8bd356f9fb691b53ce5b5677b4499c878bfa6052
bernar95 <berny.sevillista@gmail.com>
2017-06-01 23:37:35 +0200

    A pair of queries changed on review repository

--- Item 4/Acme-CinemaDB/src/main/java/repositories/ReviewRepository.java
+++ Item 4/Acme-CinemaDB/src/main/java/repositories/ReviewRepository.java
@@ -28,9 +28,9 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 	@Query("select count(r) from Review r join r.critic c group by c order by count(r) desc")
 	List<Long> findMaxReviewCritic();
 
-	@Query("select sum(r.rating)*1.0/(select count(c) from Content c where Type(c) = domain.Movie) from Review r join r.content c where Type(c) = domain.Movie")
+	@Query("select sum(r.rating)*1.0/(select count(r) from Review r join r.content c where Type(c) = domain.Movie) from Review r join r.content c where Type(c) = domain.Movie")
 	Double findAvgRatingMovies();
 
-	@Query("select sum(r.rating)*1.0/(select count(c) from Content c where Type(c) = domain.TvShow) from Review r join r.content c where Type(c) = domain.TvShow")
+	@Query("select sum(r.rating)*1.0/(select count(r) from Review r join r.content c where Type(c) = domain.TvShow) from Review r join r.content c where Type(c) = domain.TvShow")
 	Double findAvgRatingTvShows();
 }
