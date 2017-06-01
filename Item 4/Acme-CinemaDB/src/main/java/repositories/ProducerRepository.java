4ccbd7544d7c4c88870e9442390c971e18f24475
bernar95 <berny.sevillista@gmail.com>
2017-06-01 21:26:21 +0200

    Some new queries added

--- Item 4/Acme-CinemaDB/src/main/java/repositories/ProducerRepository.java
+++ Item 4/Acme-CinemaDB/src/main/java/repositories/ProducerRepository.java
@@ -1,6 +1,8 @@
 
 package repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,5 +14,11 @@ public interface ProducerRepository extends JpaRepository<Producer, Integer> {
 
 	@Query("select p from Producer p where p.userAccount.id = ?1")
 	Producer findByUserAccountId(int userAccountId);
+
+	@Query("select p.id from Producer p")
+	List<Integer> findAllProducerId();
+
+	@Query("select p.id from Movie m join m.producer p")
+	List<Integer> findAllProducerWithMoviesId();
 
 }
--- Item 4/Acme-CinemaDB/src/main/java/repositories/UserRepository.java
+++ Item 4/Acme-CinemaDB/src/main/java/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 
 package repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,5 +14,11 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	@Query("select u from User u where u.userAccount.id = ?1")
 	User findByUserAccountId(int userAccountId);
+
+	@Query("select u.id from User u")
+	List<Integer> findAllUserId();
+
+	@Query("select u.id from OrderUser o join o.user u")
+	List<Integer> findAllUsersWithOrdersId();
 
 }
