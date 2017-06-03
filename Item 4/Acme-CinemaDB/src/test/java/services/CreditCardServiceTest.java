e9513fcf22d40e12ca6ad109db06388660b19aa0
Javier Santos <me@javiersantos.me>
2017-06-03 12:18:36 +0200

    Improve CreditCard tests

--- Item 4/Acme-CinemaDB/src/test/java/services/CreditCardServiceTest.java
+++ Item 4/Acme-CinemaDB/src/test/java/services/CreditCardServiceTest.java
@@ -1,6 +1,8 @@
 
 package services;
 
+import javax.validation.ConstraintViolationException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +46,22 @@ public class CreditCardServiceTest extends AbstractTest {
 		for (int i = 0; i < testingData.length; i++)
 			this.saveCreditCardTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (int) testingData[i][4], (int) testingData[i][5], (int) testingData[i][6], (Class<?>) testingData[i][7]);
 	}
+
+	@Test
+	public void editCreditCardDriver() {
+		final Object testingData[][] = {
+			{
+				// An user edit his credit card with an invalid CVV -> Exception
+				"user3", 240, 98, ConstraintViolationException.class
+			}, {
+				// OK
+				"user3", 240, 120, null
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.editCreditCardTemplate((String) testingData[i][0], (int) testingData[i][1], (int) testingData[i][2], (Class<?>) testingData[i][3]);
+	}
 	// Ancillary methods ------------------------------------------------------
 
 	protected void saveCreditCardTemplate(final String username, final String holder, final String brand, final String number, final int year, final int month, final int cvv, final Class<?> expected) {
@@ -52,9 +70,12 @@ public class CreditCardServiceTest extends AbstractTest {
 		caught = null;
 
 		try {
+			CreditCard creditCard;
+			final CreditCard saved;
+
 			this.authenticate(username);
 
-			final CreditCard creditCard = this.creditCardService.create();
+			creditCard = this.creditCardService.create();
 			creditCard.setHolder(holder);
 			creditCard.setBrand(brand);
 			creditCard.setNumber(number);
@@ -65,10 +86,39 @@ public class CreditCardServiceTest extends AbstractTest {
 			Assert.isTrue(this.creditCardService.isCreditCardBrandValid(creditCard));
 			Assert.isTrue(this.creditCardService.isCreditCardDateValid(creditCard));
 
-			this.creditCardService.save(creditCard);
+			saved = this.creditCardService.save(creditCard);
 			this.creditCardService.flush();
 
 			this.unauthenticate();
+
+			Assert.isTrue(saved.getId() > 0);
+		} catch (final Throwable th) {
+			caught = th.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
+	}
+
+	protected void editCreditCardTemplate(final String username, final int creditCardId, final int cvv, final Class<?> expected) {
+		Class<?> caught;
+
+		caught = null;
+
+		try {
+			CreditCard creditCard;
+			final CreditCard saved;
+
+			this.authenticate(username);
+
+			creditCard = this.creditCardService.findOne(creditCardId);
+			creditCard.setCvv(cvv);
+
+			saved = this.creditCardService.save(creditCard);
+			this.creditCardService.flush();
+
+			this.unauthenticate();
+
+			Assert.isTrue(saved.getId() > 0);
 		} catch (final Throwable th) {
 			caught = th.getClass();
 		}
